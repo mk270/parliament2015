bee4afd3c5660e4904517a2b22dae21473e5a2bf
James Nocentini <james.nocentini@gmail.com>
2013-03-11 10:46:34 +0000

    feeding timeline with json

--- dbxs.js
+++ dbxs.js
@@ -1,25 +1,21 @@
 #!/usr/bin/node
-module.exports = dbxs =  function() {
+module.exports =  function() {
+
 var http = require('http');
 var sqlite3 = require('sqlite3').verbose()
 
 var db = new sqlite3.Database('node/test.db');
 
-http.createServer(function (request, response) {
 
-  var lookupEvents = function(callback) {
-	db.serialize(function() {
-	  db.all("SELECT * from event", callback);
-	});
-  };
+this.lookupEvents = function(callback) {
+    db.serialize(function() {
+        db.all("SELECT * from event", callback);
+    });
+};
 
-  var continueHttpStream = function(err, rows) {
-	response.write(JSON.stringify(rows));
-	response.end();
-  };
-
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  lookupEvents(continueHttpStream);
-
-}).listen(8080);
 }
+
+
+
+
+
--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 var express = require('express'),
     routes = require('./routes'),
     api = require('./routes/api')
-    dbxs = require('./dbxs')()
-    dbxs
+    dbxsCode = require('./dbxs')
+    dbxs = new dbxsCode()
 
 
 var app = express()
@@ -28,6 +28,45 @@ app.configure('production', function() {})
 //Routes
 app.get('/', routes.index)
 
+app.get('/events', function(req, res) {
+    //Get events from database
+    dbxs.lookupEvents(function(err, rows) {
+        events = []
+        //Change the format of the json object to match the timeline requirements
+        rows.forEach(function(row) {
+            event = {
+                startDate: "2015,12,5",
+                endDate: "2015,12,5",
+                headline: row.headline,
+                text: row.event_body,
+                tag: row.tag,
+                classname: '',
+                asset: {
+                    media: row.media,
+                    thumbnail: '',
+                    credit: row.media_credit,
+                    caption: ''
+                }
+            }
+            events.push(event)
+        })
+        var timelineJSON = {
+            timeline: {
+                headline: "Parliament 2015",
+                type: "default",
+                text: "<p>The history of Parliament since Magna Carta</p>",
+                asset: {
+                    media: "http://www.bestourism.com/img/items/big/764/Houses-of-Parliament_Houses-of-Parliament-view_3027.jpg",
+                    credit: "Developed at Rewired State Parliament Hackathon",
+                    caption: ""
+                },
+                date: events
+            }
+        }
+        res.send(timelineJSON)
+    });
+})
+
 //Api
 
 //Redirects all others to index (HTML5 history)
@@ -36,5 +75,5 @@ app.get('*', routes.index)
 //Start Server
 var port = process.env.PORT || 3000
 app.listen(port, function() {
-    console.log('Ready to hack on port '+port)
+    console.log('Server.js listening on '+port)
 })
