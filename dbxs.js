98110d6a0cd06199e5d0ce98987ec637b486b848
Martin Keegan <martin@no.ucant.org>
2013-03-11 16:58:28 +0000

    try postgres

--- dbxs.js
+++ dbxs.js
@@ -1,25 +1,35 @@
 #!/usr/bin/node
-module.exports = dbxs =  function() {
-var http = require('http');
-var sqlite3 = require('sqlite3').verbose()
 
-var db = new sqlite3.Database('node/test.db');
+var http = require('http');
+
+var pg = require('pg');
+//var conString = process.env.DATABASE_URL || "parliament2015";
+var conString = "parliament2015";
+function json_of_sql_rows(cb) {
+  var client = new pg.Client(conString);
+
+  console.log("connecting");
+  client.connect(function(err) {
+	console.log("connected");
+	client.query('SELECT * FROM event"', function(err, result) {
+	  console.log("hello?");
+      console.log(result.rows[0]);
+	  cb(JSON.stringify(result.rows));
+	});
+	console.log("query submitted");
+  });
+}
 
 http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
 
-  var lookupEvents = function(callback) {
-	db.serialize(function() {
-	  db.all("SELECT * from event", callback);
-	});
-  };
+  console.log("recv");
 
-  var continueHttpStream = function(err, rows) {
-	response.write(JSON.stringify(rows));
+  var cb = function(data) {
+	console.log("resp");
+	response.write(data);
 	response.end();
   };
 
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  lookupEvents(continueHttpStream);
-
+  json_of_sql_rows(cb);
 }).listen(8080);
-}
