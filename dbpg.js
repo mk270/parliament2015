a3d803d34679afdd0b5d7a1e1c83906d62e5dd68
createS0ftware <hanson.aboagye@higher-thoughts.co.uk>
2013-03-13 13:05:49 +0000

    Update dbpg.js

--- dbpg.js
+++ dbpg.js
@@ -6,7 +6,9 @@ module.exports = dbpg = function() {
 
     client = new pg.Client(connectionString)
     client.connect();
-    //query = client.query('CREATE TABLE event (event_id SERIAL primary key,start_date date,end_date date,headline text,event_body text,media text,media_credit text,media_caption text,timeline_event_id integer,view_id text,tag text)');
+    //query = client.query('CREATE TABLE event (event_id SERIAL primary key,start_date 
+    //date,end_date date,headline text,event_body text,media text,media_credit text,media_caption 
+    //text,timeline_event_id integer,view_id text,tag text)');
 
 
     this.lookupEvents = function (callback) {
