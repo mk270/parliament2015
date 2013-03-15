23a40dfa7146ad1d3746ff4ecf63a34d9ba1ec2c
Hanson <hanson.aboagye@higher-thoughts.co.uk>
2013-03-15 15:33:03 +0000

    Hanson added TTS support test

--- dbpg.js
+++ dbpg.js
@@ -56,7 +56,95 @@ module.exports = dbpg = function() {
 
     }
 
+    this.lookupEvents = function (callback) {
 
+
+
+        var query = client.query("SELECT * FROM event");
+        //Get events from db
+        query.on('row', function(row, result) {
+            var changeDateFormat = function(oldDate) {
+
+                var year = oldDate.getUTCFullYear()
+                var month = oldDate.getUTCMonth() + 1
+                var day = oldDate.getDate()
+                var newDate = year+","+month+","+day
+                return newDate
+
+            }
+            var newStartDate = changeDateFormat(row.start_date)
+            var newEndDate = changeDateFormat(row.end_date)
+            row = {
+                startDate: newStartDate,
+                endDate: newEndDate,
+                headline: row.headline,
+                text: row.event_body,
+                tag: row.tag,
+                classname: '',
+                asset: {
+                    media: row.media,
+                    thumbnail: '',
+                    credit: row.media_credit,
+                    caption: ''
+                }
+            }
+            result.addRow(row)
+            console.log(row)
+
+        })
+        query.on('end', callback)
+
+
+
+
+
+
+    }
+
+
+    this.lookupEvents2 = function (callback,eventid) {
+
+
+
+        var query = client.query("SELECT * FROM event where timeline_event_id = " + eventid);
+        //Get events from db
+        query.on('row', function(row, result) {
+            var changeDateFormat = function(oldDate) {
+
+                var year = oldDate.getUTCFullYear()
+                var month = oldDate.getUTCMonth() + 1
+                var day = oldDate.getDate()
+                var newDate = year+","+month+","+day
+                return newDate
+
+            }
+            var newStartDate = changeDateFormat(row.start_date)
+            var newEndDate = changeDateFormat(row.end_date)
+            row = {
+                startDate: newStartDate,
+                endDate: newEndDate,
+                headline: row.headline,
+                text: row.event_body,
+                tag: row.tag,
+                classname: '',
+                asset: {
+                    media: row.media,
+                    thumbnail: '',
+                    credit: row.media_credit,
+                    caption: ''
+                }
+            }
+            result.addRow(row)
+            console.log(row)
+
+        })
+        query.on('end', callback)
+
+
+    }
+
+
+    
     this.addEvent = function (event) {
         client.query("INSERT INTO event(start_date, end_date, headline, event_body, media, media_credit) values($1, $2, $3, $4, $5, $6)", event)
     }
